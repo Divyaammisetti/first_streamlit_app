31f9c1d9b86f8d87bcefb37c9f0b1fc02fbbecb7
Divyaammisetti <divyaammisetti12@gmail.com>
2022-08-24 11:02:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,6 @@
 import streamlit
 import requests
-import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
 streamlit.header("Fruityvice Fruit Advice!")
 
 streamlit.title('My Moms New Healthy Diner')
